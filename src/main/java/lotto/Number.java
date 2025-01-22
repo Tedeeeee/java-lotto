ffd5fd0afd1e410b8d06f9fbb78cdc8abf112b3b
Tedeeeee <gksrmf359@gmail.com>
2025-01-23 00:06:58 +0900

    feat : Number의 유효성을 체크

--- src/main/java/lotto/Number.java
+++ src/main/java/lotto/Number.java
@@ -4,10 +4,17 @@ public class Number {
     private final int number;
 
     public Number(int number) {
+        validNumber(number);
         this.number = number;
     }
 
     public int getNumber() {
         return number;
+    }
+
+    private void validNumber(int inputNumber) {
+        if (inputNumber < 1 || inputNumber > 45) {
+            throw new IllegalArgumentException("1 ~ 45 사이의 값을 입력해주세요");
+        }
     }
 }
--- src/test/java/lotto/NumberTest.java
+++ src/test/java/lotto/NumberTest.java
@@ -3,6 +3,8 @@ package lotto;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class NumberTest {
 
@@ -12,5 +14,22 @@ public class NumberTest {
         Number number = new Number(10);
 
         Assertions.assertThat(number.getNumber()).isEqualTo(10);
+    }
+
+    @ParameterizedTest
+    @DisplayName("1 ~ 45를 벗어난 숫자는 예외를 발생 시킨다")
+    @ValueSource(ints = {0, 46})
+    void numberRangeCheck(int rangeValue) {
+        Assertions.assertThatThrownBy(() -> new Number(rangeValue))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("1 ~ 45 사이의 값을 입력해주세요");
+    }
+
+    @ParameterizedTest
+    @DisplayName("1 ~ 45 사이의 숫자는 예외를 던지지 않는다")
+    @ValueSource(ints = {1, 45})
+    void numberRangeCheckNotThrowException(int rangeValue) {
+        Assertions.assertThatCode(() -> new Number(rangeValue))
+                .doesNotThrowAnyException();
     }
 }
