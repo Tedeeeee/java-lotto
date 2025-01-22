eeb37d74323dabb754a5ff3cafc6daf1b0802e92
Tedeeeee <gksrmf359@gmail.com>
2025-01-23 00:14:10 +0900

    refactor : 하드 코딩 내용 상수화

--- src/main/java/lotto/Number.java
+++ src/main/java/lotto/Number.java
@@ -1,20 +1,24 @@
 package lotto;
 
 public class Number {
+    private static final String INVALID_NUMBER_MESSAGE = "1 ~ 45 사이의 값을 입력해주세요";
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+
     private final int number;
 
     public Number(int number) {
-        validNumber(number);
+        validateNumber(number);
         this.number = number;
+    }
+
+    private void validateNumber(int inputNumber) {
+        if (inputNumber < MIN_NUMBER || inputNumber > MAX_NUMBER) {
+            throw new IllegalArgumentException(INVALID_NUMBER_MESSAGE);
+        }
     }
 
     public int getNumber() {
         return number;
-    }
-
-    private void validNumber(int inputNumber) {
-        if (inputNumber < 1 || inputNumber > 45) {
-            throw new IllegalArgumentException("1 ~ 45 사이의 값을 입력해주세요");
-        }
     }
 }
