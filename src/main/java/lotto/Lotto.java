9bfadf58bbef02e52cdfe7f4840dfb60cd3f7942
Tedeeeee <gksrmf359@gmail.com>
2025-01-24 20:25:53 +0900

    feat : 중복된 번호는 들어갈 수 없다

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,12 +1,15 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        duplicateValidation(numbers);
         this.numbers = numbers;
     }
 
@@ -16,4 +19,11 @@ public class Lotto {
         }
     }
 
+    private void duplicateValidation(List<Integer> numbers) {
+        Set<Integer> duplicates = new HashSet<>(numbers);
+
+        if (duplicates.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복이 될 수 없습니다");
+        }
+    }
 }
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -15,6 +15,12 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @Test
+    void 로또_번호의_개수가_6개보다_적다면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 5, 6, 7)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
