c5f441a5ba4762b3c80b25e15ed12963be34c991
Tedeeeee <gksrmf359@gmail.com>
2025-01-25 22:57:01 +0900

    validate 정적 메소드 리팩토링

--- src/main/java/lotto/LottoMoneyValidator.java
+++ src/main/java/lotto/LottoMoneyValidator.java
@@ -1,6 +1,7 @@
 package lotto;
 
 public class LottoMoneyValidator {
+    private static final int LOTTO_PRICE_UNIT = 1000;
 
     public static int validateThousandUnit(String inputMoney) {
 
@@ -8,26 +9,25 @@ public class LottoMoneyValidator {
             throw new IllegalArgumentException("소수점 값은 입력할 수 없습니다");
         }
 
-        int money = 0;
         try {
-            money = Integer.parseInt(inputMoney);
+            int money = Integer.parseInt(inputMoney);
+
+            if (money < 0) {
+                throw new IllegalArgumentException("양수의 금액을 입력해주세요");
+            }
+
+            if ((money / 1000) <= 0) {
+                throw new IllegalArgumentException("로또를 한 개 이상 구매해주세요");
+            }
+
+            if ((money % 1000) > 0) {
+                throw new IllegalArgumentException("천원 단위의 금액을 입력해주세요");
+            }
+
+            return money / 1000;
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("정상적인 금액을 입력해주세요");
         }
-
-        if (money < 0) {
-            throw new IllegalArgumentException("양수의 금액을 입력해주세요");
-        }
-
-        if ((money / 1000) <= 0) {
-            throw new IllegalArgumentException("로또를 한 개 이상 구매해주세요");
-        }
-
-        if ((money % 1000) > 0) {
-            throw new IllegalArgumentException("천원 단위의 금액을 입력해주세요");
-        }
-
-        return money / 1000;
     }
 
 
