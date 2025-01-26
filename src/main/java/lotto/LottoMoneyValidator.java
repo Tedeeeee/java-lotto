a303378cc99c2bd72eda783e39f47f1c6d5393ac
Tedeeeee <gksrmf359@gmail.com>
2025-01-26 16:09:17 +0900

    사용자의 입력값의 유효성을 검증하는 로직 추가

--- src/main/java/lotto/LottoMoneyValidator.java
+++ src/main/java/lotto/LottoMoneyValidator.java
@@ -3,32 +3,37 @@ package lotto;
 public class LottoMoneyValidator {
     private static final int LOTTO_PRICE_UNIT = 1000;
 
-    public static int validateThousandUnit(String inputMoney) {
+    public static int calculateLottoCount(String inputMoney) {
+        int money = parseInt(inputMoney);
+        if ((money / LOTTO_PRICE_UNIT) <= 0) {
+            throw new IllegalArgumentException("로또를 한 개 이상 구매해주세요");
+        }
 
-        if (inputMoney.contains(".")) {
+        if ((money % LOTTO_PRICE_UNIT) > 0) {
+            throw new IllegalArgumentException("천원 단위의 금액을 입력해주세요");
+        }
+        return money / LOTTO_PRICE_UNIT;
+    }
+
+    public static int validateBonusNumber(String inputBonusNumber) {
+        return parseInt(inputBonusNumber);
+    }
+
+    private static int parseInt(String input) {
+        if (input.contains(".")) {
             throw new IllegalArgumentException("소수점 값은 입력할 수 없습니다");
         }
 
         try {
-            int money = Integer.parseInt(inputMoney);
+            int value = Integer.parseInt(input);
 
-            if (money < 0) {
+            if (value < 0) {
                 throw new IllegalArgumentException("양수의 금액을 입력해주세요");
             }
 
-            if ((money / 1000) <= 0) {
-                throw new IllegalArgumentException("로또를 한 개 이상 구매해주세요");
-            }
-
-            if ((money % 1000) > 0) {
-                throw new IllegalArgumentException("천원 단위의 금액을 입력해주세요");
-            }
-
-            return money / 1000;
+            return value;
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("정상적인 금액을 입력해주세요");
         }
     }
-
-
 }
--- src/test/java/lotto/LottoMoneyValidatorTest.java
+++ src/test/java/lotto/LottoMoneyValidatorTest.java
@@ -8,8 +8,14 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 class LottoMoneyValidatorTest {
 
-    private void assertThrowWithMessage(String inputMoney, String expectedMessage) {
-        Assertions.assertThatThrownBy(() -> LottoMoneyValidator.validateThousandUnit(inputMoney))
+    private void assertThrowWithMessageByChangeMoney(String inputMoney, String expectedMessage) {
+        Assertions.assertThatThrownBy(() -> LottoMoneyValidator.calculateLottoCount(inputMoney))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(expectedMessage);
+    }
+
+    private void assertThrowWithMessageByChangeBonusNumber(String inputBonusNumber, String expectedMessage) {
+        Assertions.assertThatThrownBy(() -> LottoMoneyValidator.validateBonusNumber(inputBonusNumber))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(expectedMessage);
     }
@@ -17,39 +23,55 @@ class LottoMoneyValidatorTest {
     @ParameterizedTest
     @ValueSource(strings = {"1500", "2001", "1999"})
     void 천원단위의_돈이_아니면_예외를_발생(String money) {
-        assertThrowWithMessage(money, "천원 단위의 금액을 입력해주세요");
+        assertThrowWithMessageByChangeMoney(money, "천원 단위의 금액을 입력해주세요");
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"-1000", "-1", "-999", "-5000"})
-    void 음수의_돈이_들어오면_예외_발생(String money) {
-        assertThrowWithMessage(money, "양수의 금액을 입력해주세요");
+    void 결제_금액이_음수의_돈으로_들어오면_예외_발생(String money) {
+        assertThrowWithMessageByChangeMoney(money, "양수의 금액을 입력해주세요");
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"abcd", "금액", "one", "!@#", "1000원", "천원", "one thousand", "1234,56", "", " "})
-    void 숫자가_아닌_다른_값을_입력하면_예외_발생(String money) {
-        assertThrowWithMessage(money, "정상적인 금액을 입력해주세요");
+    void 결제_금액이_숫자가_아닌_다른_값을_입력하면_예외_발생(String money) {
+        assertThrowWithMessageByChangeMoney(money, "정상적인 금액을 입력해주세요");
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"1000.5", "0.001", "1234.56", "-1000.5"})
-    void 소수_값이_들어오면_예외_발생(String money) {
-        assertThrowWithMessage(money, "소수점 값은 입력할 수 없습니다");
+    void 결제_금액이_소수_값으로_들어오면_예외_발생(String money) {
+        assertThrowWithMessageByChangeMoney(money, "소수점 값은 입력할 수 없습니다");
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"1", "999", "0"})
     void 한개도_사지_않으면_예외_발생(String money) {
-        assertThrowWithMessage(money, "로또를 한 개 이상 구매해주세요");
+        assertThrowWithMessageByChangeMoney(money, "로또를 한 개 이상 구매해주세요");
     }
 
     @ParameterizedTest
     @CsvSource(value = {"1000, 1", "2000, 2"})
     void 정상적인_금액을_입력하면_로또의_갯수를_반환(String money, int lottoCount) {
-        Assertions.assertThat(LottoMoneyValidator.validateThousandUnit(money))
+        Assertions.assertThat(LottoMoneyValidator.calculateLottoCount(money))
                 .isEqualTo(lottoCount);
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"-1", "-45", "-37"})
+    void 보너스_번호가_음수의_값으로_들어오면_예외_발생(String money) {
+        assertThrowWithMessageByChangeBonusNumber(money, "양수의 금액을 입력해주세요");
+    }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"abcd", "금액", "one", "!@#", "1000원", "천원", "one thousand", "1234,56", "", " "})
+    void 보너스_숫자가_숫자가_아닌_다른_값을_입력하면_예외_발생(String money) {
+        assertThrowWithMessageByChangeBonusNumber(money, "정상적인 금액을 입력해주세요");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1.1", "45.1", "23.56"})
+    void 보너스_값을_소수_값으로_들어오면_예외_발생(String money) {
+        assertThrowWithMessageByChangeBonusNumber(money, "소수점 값은 입력할 수 없습니다");
+    }
 }
